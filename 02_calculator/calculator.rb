10cdeffcad68878687b590f3bae843e5280fdc82
MikeKochendorfer <mike.kochendorfer@gmail.com>
2020-02-10 17:22:20 -0700

    First round of tests passing.  Bonus tests to be completed.

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,18 @@
+def add(num1, num2)
+	return num1 + num2
+end
+
+def subtract(num1, num2)
+	return num1 - num2
+end
+
+def sum(array)
+	total = 0
+	array.each do |i|
+		total += i
+	end
+	return total
+end
+
+
 #write your code here
