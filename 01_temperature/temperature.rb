4f5ddb26b67e9c03d5f10485e0a3d5da97b2fb4b
MikeKochendorfer <mike.kochendorfer@gmail.com>
2020-02-10 17:12:14 -0700

    All test passing

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,1 +1,10 @@
+def ftoc(tempF)
+	converted_tempurature = (tempF -32) * (5.0/9.0)
+	return converted_tempurature
+end
+
+def ctof(tempC)
+	converted_tempurature = tempC * (9.0/5.0) + 32
+	return  converted_tempurature
+end
 #write your code here
