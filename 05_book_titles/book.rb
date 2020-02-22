52ad77e6d0c12dba8c7ef4bc33be42487c61215a
MikeKochendorfer <mike.kochendorfer@gmail.com>
2020-02-21 18:02:42 -0700

    All tests passing

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,20 @@
 class Book
-# write your code here
+	attr_reader :title
+
+	def title=(string)
+		string_split = string.split
+		titleized = []
+		not_capitalized = ["and","the","over","a","an","in","of"]
+		string_split.each do |word|
+			if not_capitalized.include? word
+				titleized.push(word)
+			else
+				titleized.push(word.capitalize)
+			end
+		end
+		titleized[0] = titleized[0].capitalize
+		@title = titleized.join(" ")
+	end
 end
+
+
